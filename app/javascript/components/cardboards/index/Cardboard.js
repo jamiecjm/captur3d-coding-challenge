b02e47dc327ab6b9ed5ae69793ea3124957afcdd
jiemeechong <jamiecjm@gmail.com>
2018-05-28 01:30:57 +1000

    Minor adjustment

--- app/javascript/components/cardboards/index/Cardboard.js
+++ app/javascript/components/cardboards/index/Cardboard.js
@@ -1,13 +1,39 @@
 import React from "react"
 import PropTypes from "prop-types"
+import AddToCart from '../AddToCart'
+
 class Cardboard extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      quantity: 1
+    };
+  }
+
+  handleUserInput(obj){
+    this.setState(obj)
+  }
+
+  handleFormSubmit(){
+    var line_item = {cardboard_id: this.props.cardboard.id, quantity: this.state.quantity}
+    $.post('/line_items',{
+      line_item: line_item
+    });
+  }
+
   render () {
     return (
       <React.Fragment>
-      <a href={this.props.cardboard.url} className='cardboard-item'>
-        <h3>{this.props.cardboard.cardboard_type}</h3>
-        <p>{this.props.cardboard.price}</p>
-      </a>
+      <div className='cardboard-item'>
+        <a href={this.props.cardboard.url}>
+          <h3>{this.props.cardboard.cardboard_type}</h3>
+          <p>{this.props.cardboard.price}</p>
+        </a>
+        <AddToCart quantity={this.state.quantity}
+        onInput={this.handleUserInput.bind(this)}
+        onFormSubmit={this.handleFormSubmit.bind(this)}/>
+      </div>
       </React.Fragment>
     );
   }
