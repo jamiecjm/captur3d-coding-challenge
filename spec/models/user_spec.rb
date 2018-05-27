109b9278025b8383e139afde245b6983c5461b2f
jiemeechong <jamiecjm@gmail.com>
2018-05-27 18:10:48 +1000

    Complete user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = create(:user)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'has a unique email' do
+    user2 = build(:user, password: 'user123456', password_confirmation: 'user123456')
+    expect(user2).to_not be_valid
+  end
+end
