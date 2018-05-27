404cda4bd9be92d23156e99af0f5e3d619084083
jiemeechong <jamiecjm@gmail.com>
2018-05-27 15:04:30 +1000

    Add requirements to promotions

--- db/migrate/20180527044150_create_promotions.rb
+++ db/migrate/20180527044150_create_promotions.rb
@@ -3,12 +3,15 @@ class CreatePromotions < ActiveRecord::Migration[5.1]
     create_table :promotions do |t|
       t.integer :promotion_type, null: false
       t.decimal :discount_amount, precision: 10, scale: 2, default: "0.0", null: false
+      t.string :requirement_field
+      t.string :requirement_operator, default: 'mt'
+      t.integer :requirement_amount, default: 0, null: false
       t.text :description
       t.timestamps
     end
   end
 
-  def drop
+  def down
     drop_table :promotions
   end
 end
