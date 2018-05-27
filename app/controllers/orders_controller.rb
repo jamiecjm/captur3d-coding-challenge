e18258750d14a1ca63a9547d71f4a6cba9d4b662
jiemeechong <jamiecjm@gmail.com>
2018-05-27 13:11:51 +1000

    Create order controller to display order placed by user

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show]
+
+  before_action :authenticate_user!
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.fetch(:order, {})
+    end
+end
